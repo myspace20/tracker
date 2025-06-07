269a152c55ad879f6cb361d9ff106b060c6f72ad
rogersatsi <rogersatsi98@gmail.com>
2025-06-07 15:40:07 +0000

    feat(validations): add exception handler for request bodies

--- src/main/java/com/task/tracker/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/task/tracker/exceptions/GlobalExceptionHandler.java
@@ -3,16 +3,36 @@ package com.task.tracker.exceptions;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 
 import java.time.LocalDateTime;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    public ResponseEntity<ApiErrorResponse> handleValidationError(
+            MethodArgumentNotValidException ex,
+            WebRequest request) {
+        String errors = ex.getBindingResult()
+                .getFieldErrors()
+                .stream()
+                .map(err -> err.getField() + ": " + err.getDefaultMessage())
+                .collect(Collectors.joining("; "));
+
+        return new ResponseEntity<>(
+        new ApiErrorResponse(
+                LocalDateTime.now(),
+                errors,
+                HttpStatus.UNPROCESSABLE_ENTITY.value(),
+                request.getDescription(false)
+        ), HttpStatus.UNPROCESSABLE_ENTITY
+        );
+    }
 
     @ExceptionHandler(ResourceNotFound.class)
     public ResponseEntity<Object> handleResourceNotFound(ResourceNotFound ex, WebRequest request) {
