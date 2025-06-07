42dbe05aa03ae1a3e6385c3d8f5be2e57bad1bed
rogersatsi <rogersatsi98@gmail.com>
2025-06-07 20:07:33 +0000

    feat(util): add static payload creator

--- src/main/java/com/task/tracker/util/ObjectToPayload.java
+++ src/main/java/com/task/tracker/util/ObjectToPayload.java
@@ -1,4 +1,12 @@
 package com.task.tracker.util;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class ObjectToPayload {
+    public static Map<String, Object> createPayload(String key, Object value) {
+        Map<String, Object> payload = new HashMap<>();
+        payload.put(key, value);
+        return payload;
+    }
 }
