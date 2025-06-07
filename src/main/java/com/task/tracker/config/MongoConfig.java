334c08ed17b8c140f308c2c08ffe21c02042c7cc
rogersatsi <rogersatsi98@gmail.com>
2025-06-07 20:05:28 +0000

    feat(config): mongo db config for date conversions

--- src/main/java/com/task/tracker/config/MongoConfig.java
+++ src/main/java/com/task/tracker/config/MongoConfig.java
@@ -1,4 +1,35 @@
 package com.task.tracker.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.data.mongodb.core.convert.MongoCustomConversions;
+
+import java.sql.Date;
+import java.util.Arrays;
+
+@Configuration
 public class MongoConfig {
+
+    private static class SqlDateToUtilDateConverter implements Converter<Date, java.util.Date> {
+        @Override
+        public java.util.Date convert(Date source) {
+            return new java.util.Date(source.getTime());
+        }
+    }
+
+    private static class UtilDateToSqlDateConverter implements Converter<java.util.Date, Date> {
+        @Override
+        public Date convert(java.util.Date source) {
+            return new Date(source.getTime());
+        }
+    }
+
+    @Bean
+    public MongoCustomConversions customConversions() {
+        return new MongoCustomConversions(Arrays.asList(
+                new SqlDateToUtilDateConverter(),
+                new UtilDateToSqlDateConverter()
+        ));
+    }
 }
