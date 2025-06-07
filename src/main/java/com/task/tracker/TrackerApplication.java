287260fe228d4a877118ef97c010a4befa9c9794
rogersatsi <rogersatsi98@gmail.com>
2025-06-07 14:06:17 +0000

    feat(core): add caching and scheduling(cron job)

--- src/main/java/com/task/tracker/TrackerApplication.java
+++ src/main/java/com/task/tracker/TrackerApplication.java
@@ -1,9 +1,15 @@
 package com.task.tracker;
 
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.scheduling.annotation.EnableScheduling;
+
 
 @SpringBootApplication
+@EnableCaching
+@EnableScheduling
 public class TrackerApplication {
 
     public static void main(String[] args) {
