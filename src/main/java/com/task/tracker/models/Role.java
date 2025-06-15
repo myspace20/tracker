1639c435a2eac12f4eeec12aecc345abff2f111b
rogersatsi <rogersatsi98@gmail.com>
2025-06-15 15:27:02 +0000

    feat: add user role entity to facilitate relation between users and assigned roles

---
+++ src/main/java/com/task/tracker/models/Role.java
@@ -0,0 +1,43 @@
+package com.task.tracker.models;
+
+
+import jakarta.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+    }
+
+    public Role() {}
+
+    @ManyToMany(mappedBy = "roles")
+    private Set<User> users = new HashSet<>();
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+}
