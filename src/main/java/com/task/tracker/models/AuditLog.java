d922b3042ebfc59cc2642f7673f3e6b4f4c16fcf
rogersatsi <rogersatsi98@gmail.com>
2025-06-15 16:19:13 +0000

    chore: clean unused and dead code

--- src/main/java/com/task/tracker/models/AuditLog.java
+++ src/main/java/com/task/tracker/models/AuditLog.java
@@ -1,11 +1,8 @@
 package com.task.tracker.models;
 
-import com.fasterxml.jackson.annotation.JsonFormat;
-import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
-import org.springframework.data.mongodb.core.mapping.FieldType;
 
 import java.time.Instant;
 import java.util.Map;
