533ea9deed52a3c720a25bf110437ef76e52a6a8
rogersatsi <rogersatsi98@gmail.com>
2025-06-24 16:12:11 +0000

    feat(cache): add configuration for redis and caffeine caching mecahnism using dev and prod profiles

--- src/main/java/com/task/tracker/config/CacheConfig.java
+++ src/main/java/com/task/tracker/config/CacheConfig.java
@@ -1,4 +1,90 @@
 package com.task.tracker.config;
 
+
+import com.fasterxml.jackson.annotation.JsonTypeInfo;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.jsontype.impl.LaissezFaireSubTypeValidator;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import io.lettuce.core.resource.ClientResources;
+import io.micrometer.observation.ObservationRegistry;
+import org.springframework.boot.autoconfigure.cache.RedisCacheManagerBuilderCustomizer;
+import org.springframework.cache.CacheManager;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.caffeine.CaffeineCacheManager;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.data.redis.cache.RedisCacheConfiguration;
+import org.springframework.data.redis.cache.RedisCacheManager;
+import org.springframework.data.redis.connection.RedisConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceClientConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
+import org.springframework.data.redis.connection.lettuce.observability.MicrometerTracingAdapter;
+import org.springframework.data.redis.serializer.GenericJackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+
+@Configuration
+@EnableCaching
 public class CacheConfig {
+
+    List<String> cacheNames = new ArrayList<>(
+            List.of(
+                    "tasks",
+                    "projects"
+            )
+    );
+
+    @Bean
+    @Profile("dev")
+    public CacheManager caffeineCacheManager() {
+        CaffeineCacheManager cacheManager = new CaffeineCacheManager();
+        cacheManager.setCaffeine(caffeineBuilder());
+        cacheManager.setCacheNames(cacheNames);
+        return cacheManager;
+    }
+
+    @Bean
+    public Caffeine<Object, Object>  caffeineBuilder() {
+        return Caffeine.newBuilder()
+                .initialCapacity(200)
+                .maximumSize(500)
+                .recordStats();
+    }
+
+
+
+
+    @Bean
+    public GenericJackson2JsonRedisSerializer redisSerializer() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.registerModule(new JavaTimeModule());
+        objectMapper.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        objectMapper.activateDefaultTyping(
+                LaissezFaireSubTypeValidator.instance,
+                ObjectMapper.DefaultTyping.NON_FINAL,
+                JsonTypeInfo.As.PROPERTY
+        );
+        return new GenericJackson2JsonRedisSerializer(objectMapper);
+    }
+
+
+
+    @Bean
+    @Profile("prod")
+    public RedisCacheConfiguration redisCacheConfiguration(GenericJackson2JsonRedisSerializer redisSerializer) {
+        return RedisCacheConfiguration.defaultCacheConfig()
+                .entryTtl(Duration.ofSeconds(30))
+                .disableCachingNullValues()
+                .serializeValuesWith(
+                        RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer)
+                );
+    }
+
+
 }
