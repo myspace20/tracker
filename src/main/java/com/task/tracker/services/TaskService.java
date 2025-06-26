11639bfda670470864dffd6c94483b1522a08f21
rogersatsi <rogersatsi98@gmail.com>
2025-06-26 18:58:24 +0000

    feat: add caching to tasks read heavy and required methods

--- src/main/java/com/task/tracker/services/TaskService.java
+++ src/main/java/com/task/tracker/services/TaskService.java
@@ -1,20 +1,18 @@
 package com.task.tracker.services;
 
 import com.task.tracker.infrastructure.repositories.postgres.TaskRepository;
-import com.task.tracker.models.Project;
 import com.task.tracker.models.Task;
 
 import java.util.List;
-import java.util.Optional;
 
 public interface TaskService {
     List<Task> getTasks(int pageNo, int pageSize, String sortBy);
     Task getTaskById(Long id);
     Task saveTask(Task task);
-    void updateTask(Long id, Task task);
+    Task updateTask(Long id, Task task);
     void deleteTask(Long id);
-    void assignTaskToDeveloper(Long taskId, Long userId);
-    void assignTaskToProject(Long taskId, Long projectId);
+    Task assignTaskToDeveloper(Long taskId, Long userId);
+    Task assignTaskToProject(Long taskId, Long projectId);
     List<Task> getTasksByProject(Long projectId);
     List<Task> getTasksByDeveloper(Long developerId);
     void findOverdueTasks();
