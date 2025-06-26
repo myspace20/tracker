f10673d80174dd83a8c4af29e535082fb300b33c
rogersatsi <rogersatsi98@gmail.com>
2025-06-26 18:54:51 +0000

    chore: update updateProject method return value signature

--- src/main/java/com/task/tracker/services/ProjectService.java
+++ src/main/java/com/task/tracker/services/ProjectService.java
@@ -8,7 +8,7 @@ public interface ProjectService {
     List<Project> getAllProjects(int page, int size);
     Project getProjectById(Long id);
     Project createProject(Project project);
-    void updateProject(Long id, Project project);
+    Project updateProject(Long id, Project project);
     void deleteProject(Long id);
     List<Project> findProjectsWithoutTasks();
 }
